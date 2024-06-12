9759b7e95de7bbd7d3ef4e01d662a960ab3ce8be
Tuan Nguyen <tuan.nat915@gmail.com>
2024-06-12 16:29:58 -0400

    updated Action

--- lib/action/index.ts
+++ lib/action/index.ts
@@ -1,22 +1,20 @@
 export const getAllOrders = async () => {
-  const res = await fetch(`/api/orders`)
+  const res = await fetch(`${process.env.ADMIN_STORE_URL}/api/orders`)
   const data = await res.json()
   return data.orders
 }
 
 export const getOrderById = async (id: string) => {
-  const res = await fetch(`/api/orders/${id}`)
+  const res = await fetch(`${process.env.ADMIN_STORE_URL}/api/orders/${id}`)
   const data = await res.json()
   return data
 }
 
 export const getAllCustomers = async () => {
-  const res = await fetch(`/api/customers`)
+  const res = await fetch(`${process.env.ADMIN_STORE_URL}/api/customers`)
   const data = await res.json()
   return data.customers
 }
-
-
 
 export const voidEnterKey = (
   e:
